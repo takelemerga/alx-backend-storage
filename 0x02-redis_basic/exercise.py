40ecc267613d1b3d1831eebb7b1d43f8199bf8f3
tolaa <mgamtaa@gmail.com>
2023-01-18 18:16:36 +0300

    class added

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+python module interact with redis server
+"""
+import uuid
+import redis
+
+
+class Cache:
+    def __init__(self):
+        self.__redis = redis.Redis()
+        self.__redis.flush()
+
+
+def store(data):
+    """generate random key and return it"""
+    r_key = str(uuid.uuid4())
+    self.__redis.set(r_key, data)
+    return r_key
