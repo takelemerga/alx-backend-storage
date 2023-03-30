ac5fb667966ca48dacc6ed4ed513433bd14063ea
tolaa <mgamtaa@gmail.com>
2023-03-30 14:31:34 +0300

    improve task 12

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+""" MongoDB Operations with Python using pymongo """
+from pymongo import MongoClient
+
+
+def logStats():
+    """print stats about Nginx logs stored in MongoDB"""
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+
+    n_logs = nginx_collection.count_documents({})
+    print(f'{n_logs} logs')
+
+    print('Methods:')
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f'\tmethod {method}: {count}')
+
+    status_check = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+
+    print(f'{status_check} status check')
+
+    print('IPs:')
+    request_logs = server_collection.aggregate(
+        [
+            {
+                '$group': {'_id': "$ip", 'totalRequests': {'$sum': 1}}
+            },
+            {
+                '$sort': {'totalRequests': -1}
+            },
+            {
+                '$limit': 10
+            },
+        ]
+    )
+    for request_log in request_logs:
+        ip = request_log['_id']
+        count = request_log['totalRequests']
+        print(f'\t{ip}: {count}')
+
+
+if __name__ == "__main__":
+    logStats()
